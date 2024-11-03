29e6ea1a2dccd686102f354aa1b853effd8a963c
Owner <divij.seedat@gmail.com>
2024-11-03 12:21:20 +1100

    Added Level1 button functionality

---
+++ Assets/Scripts/UI_Manager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public void LoadFirstLevel()
+    {
+        DontDestroyOnLoad(gameObject);
+        SceneManager.LoadScene("Manual_Level");
+        
+        
+    }
+}
