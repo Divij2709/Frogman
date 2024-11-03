4286ace6fe5923b3851f5b5739ea400d2316043a
Owner <divij.seedat@gmail.com>
2024-11-03 14:15:08 +1100

    Added most of the UI, Need to fix the ghost numbering. Exit button works

--- Assets/Scripts/UI_Manager.cs
+++ Assets/Scripts/UI_Manager.cs
@@ -12,4 +12,23 @@ public class UIManager : MonoBehaviour
         
         
     }
+    
+    public void ExitGame()
+    {
+        DontDestroyOnLoad(gameObject);
+        SceneManager.LoadScene("StartScene");
+        
+    }
+    
+    public void QuitGame()
+    {
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+                
+        #if UNITY_STANDALONE
+                Application.Quit();
+        #endif
+        
+    }
 }
